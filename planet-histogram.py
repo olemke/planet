63008432a9d198257dd4ea6f83f536d7cba7901a
Oliver Lemke <olemke@gmail.com>
2025-03-04 09:17:25 +0100

    Add option to histogram for downloaded images only

--- planet-histogram.py
+++ planet-histogram.py
@@ -10,22 +10,29 @@ import pandas as pd
 import planet_download as planet
 
 
-def plot_histogram(filename):
+def plot_histogram(filename, only_downloaded=False):
     with open(filename, "r") as f:
         items = json.load(f)
     name = planet.get_config_basename(filename)
 
-    timestamps = [
-        datetime.strptime(ts["properties"]["acquired"], "%Y-%m-%dT%H:%M:%S.%fZ")
-        for ts in items["results"]
-    ]
+    if only_downloaded:
+        timestamps = [
+            datetime.strptime(ts["properties"]["acquired"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            for ts in items["results"]
+            if planet.check_existence(ts["id"], items["config"]["download_path"])
+        ]
+    else:
+        timestamps = [
+            datetime.strptime(ts["properties"]["acquired"], "%Y-%m-%dT%H:%M:%S.%fZ")
+            for ts in items["results"]
+        ]
 
     df = pd.DataFrame(timestamps, columns=["timestamp"])
     doy = df.groupby(df["timestamp"].dt.dayofyear)
 
     fig, ax = plt.subplots(figsize=(3 + len(doy) / 5, 5), tight_layout=True)
     doy.count().plot(ax=ax, kind="bar")
-    ax.set_title(f"{items['config']['description']} ({len(items['results'])} images)")
+    ax.set_title(f"{items['config']['description']} ({len(timestamps)} images)")
     ax.set_ylabel("Number of images")
     ax.set_xlabel("Day of year")
 
@@ -35,8 +42,14 @@ def plot_histogram(filename):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} SEARCH_RESULTS_FILE.json")
+    if len(sys.argv) < 2:
+        print(f"Usage: {sys.argv[0]} SEARCH_RESULTS_FILE.json [-d]")
+        print("")
+        print("     -d  Only plot downloaded images")
         exit(1)
+   
+    only_downloaded = False
+    if len(sys.argv) > 2 and sys.argv[2] == "-d":
+        only_downloaded = True
 
-    plot_histogram(sys.argv[1])
+    plot_histogram(sys.argv[1], only_downloaded)
