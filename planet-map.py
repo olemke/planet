95f76171f7a472db4f9a4e182676faa6fe1988b7
Oliver Lemke <olemke@gmail.com>
2025-03-06 09:23:25 +0100

    With -d create two subplots for downloaded and available images

--- planet-map.py
+++ planet-map.py
@@ -11,29 +11,7 @@ from shapely.geometry.polygon import Polygon
 import planet_download as planet
 
 
-def plot_map(filename, only_downloaded=False):
-    with open(filename, "r") as f:
-        items = json.load(f)
-    name = planet.get_config_basename(filename)
-
-    if only_downloaded:
-        config = items["config"]
-        locations = [
-            Polygon(ts["geometry"]["coordinates"][0])
-            for ts in items["results"]
-            if planet.check_existence(ts["id"], items["config"]["download_path"])
-        ]
-    else:
-        if "results" in items:
-            config = items["config"]
-            locations = [
-                Polygon(ts["geometry"]["coordinates"][0]) for ts in items["results"]
-            ]
-        else:
-            config = items
-            locations = []
-
-    fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
+def plot_footprints(config, locations, ax):
     ax.set_extent(
         [
             config["area_lon1"] - 1,
@@ -64,13 +42,62 @@ def plot_map(filename, only_downloaded=False):
         rasterized=True,
     )
 
-    ax.gridlines(visible=False, draw_labels=True)
+    ax.mygridliner = ax.gridlines(visible=False, draw_labels=True)
 
     ax.add_feature(cfeature.COASTLINE)
 
     ax.set_xlabel("Longitude")
     ax.set_ylabel("Latitude")
-    ax.set_title(f"{config['description']} ({len(locations)} images)")
+    ax.set_title(
+        f"{config['description']}" + (f"({len(locations)} images)" if locations else "")
+    )
+
+
+def plot_map(filename, downloaded=False):
+    with open(filename, "r") as f:
+        items = json.load(f)
+    name = planet.get_config_basename(filename)
+
+    if downloaded and "results" in items:
+        config = items["config"]
+        locations_dl = [
+            Polygon(ts["geometry"]["coordinates"][0])
+            for ts in items["results"]
+            if planet.check_existence(ts["id"], items["config"]["download_path"])
+        ]
+    else:
+        downloaded = False
+
+    if "results" in items:
+        config = items["config"]
+        locations = [
+            Polygon(ts["geometry"]["coordinates"][0]) for ts in items["results"]
+        ]
+    else:
+        config = items
+        locations = []
+
+    if downloaded:
+        fig, (ax1, ax2) = plt.subplots(
+            1,
+            2,
+            subplot_kw={"projection": ccrs.PlateCarree()},
+            layout="constrained",
+            figsize=(10, 5),
+            sharey=True,
+        )
+        plot_footprints(config, locations_dl, ax1)
+        ax1.mygridliner.right_labels = False
+        plot_footprints(config, locations, ax2)
+        ax2.mygridliner.left_labels = False
+        fig.suptitle(
+            f"{config['description']} ({len(locations_dl)} out of {len(locations)} images downloaded)"
+        )
+        ax1.set_title("Downloaded")
+        ax2.set_title("Available")
+    else:
+        fig, ax = plt.subplots(subplot_kw={"projection": ccrs.PlateCarree()})
+        plot_footprints(config, locations, ax)
 
     fig.savefig(name + "-map.pdf", dpi=300)
 
@@ -83,10 +110,10 @@ if __name__ == "__main__":
     if len(sys.argv) < 2:
         print(f"Usage: {sys.argv[0]} SEARCH_RESULTS_FILE.json [-d]")
         print("")
-        print("     -d  Only plot downloaded images")
+        print("     -d  Plot both downloaded and available images")
         exit(1)
 
-    only_downloaded = False
+    downloaded = False
     if len(sys.argv) > 2 and sys.argv[2] == "-d":
-        only_downloaded = True
-    plot_map(sys.argv[1], only_downloaded)
+        downloaded = True
+    plot_map(sys.argv[1], downloaded)
